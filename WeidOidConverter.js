8682559c1df292e465691b56180d0c5b50f7d804
Daniel Marschall <28412477+danielmarschall@users.noreply.github.com>
2026-01-24 19:24:04 +0100

    Update WeidOidConverter.js

--- WeidOidConverter.js
+++ WeidOidConverter.js
@@ -4,7 +4,7 @@
 /**
 * WEID<=>OID Converter
 * (c) Webfan.de, ViaThinkSoft
-* Revision 2025-12-28
+* Revision 2026-01-24
 **/
 
 // What is a WEID?
@@ -38,6 +38,8 @@
 var WeidOidConverter = {
 
 	weLuhnCheckDigit: function(str) {
+		str = str.replace(/-+$/, '');
+
 		// Padding zeros don't count to the check digit (December 2021)
 		var ary = str.split('-');
 		ary.forEach(function (o,i,a) {
